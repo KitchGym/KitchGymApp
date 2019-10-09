651a8136fc6d1a3a86995f21eb7a76b9c8bd91c4
Kiara Boone <kljb123@gmail.com>
2019-10-09 10:59:41 -0400

    Add React web app

---
+++ kitchgym/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { 
+  makeStyles,
+  Button, 
+  TextField 
+} from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+}));
+
+function App() {
+  const {classes} = useStyles();
+  return (
+    <div className="App">
+      <h1 className="App-header">
+        Welcome to KitchGym!
+      </h1>
+      <h2 className="App-header">
+        Made to assist you along your health and fitness journey.
+      </h2>
+      <TextField
+        id="standard-name"
+        label="Name"
+        className="inputField"
+        margin="normal"
+      />
+    </div>
+  );
+}
+
+export default App;
