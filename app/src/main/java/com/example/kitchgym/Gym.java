1d29de8afeef37be527c1cc42a51119c608b26df
jarednrice <jarednrice@gmail.com>
2019-10-20 20:09:51 -0400

    Added functionality to "create workout" button. fixed visual constraints on create workout page.

--- app/src/main/java/com/example/kitchgym/Gym.java
+++ app/src/main/java/com/example/kitchgym/Gym.java
@@ -2,7 +2,9 @@ package com.example.kitchgym;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Gym extends AppCompatActivity {
 
@@ -11,4 +13,14 @@ public class Gym extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gym);
     }
+
+    public void switchToMuscleGroups(View view) {
+        Intent intent = new Intent(this, muscleGroups.class);
+        startActivity(intent);
+    }
+
+    /*public void switchToMuscleGroups(View view) {
+        Intent intent = new Intent(this, muscleGroups.class);
+        startActivity(intent);
+    }*/
 }
