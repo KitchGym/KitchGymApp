a007611e50108efede2a9693754b2cdca0f12a89
Kiara Boone <kljb123@gmail.com>
2019-11-04 09:20:18 -0500

    Add functionality to Gym side buttons

--- app/src/main/java/com/example/kitchgym/generateWorkout.java
+++ app/src/main/java/com/example/kitchgym/generateWorkout.java
@@ -2,7 +2,9 @@ package com.example.kitchgym;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class generateWorkout extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class generateWorkout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_workout);
+    }
+
+    public void switchToDoWorkout(View view) {
+        Intent intent = new Intent(this, doWorkout.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/kitchgym/muscleGroups.java
+++ app/src/main/java/com/example/kitchgym/muscleGroups.java
@@ -2,7 +2,9 @@ package com.example.kitchgym;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class muscleGroups extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class muscleGroups extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_muscle_groups);
+    }
+
+    public void switchToGenerateWorkout(View view) {
+
+        //WORKOUT GENERATION HERE//
+
+        Intent intent = new Intent(this, generateWorkout.class);
+        startActivity(intent);
     }
 }
