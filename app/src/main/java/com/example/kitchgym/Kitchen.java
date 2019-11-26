6b5db016b92271ebe77820001f2bf17bf48da62d
Kiara Boone <kljb123@gmail.com>
2019-11-26 13:20:15 -0500

    Add Input Food Activity

--- app/src/main/java/com/example/kitchgym/Kitchen.java
+++ app/src/main/java/com/example/kitchgym/Kitchen.java
@@ -2,7 +2,9 @@ package com.example.kitchgym;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Kitchen extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class Kitchen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kitchen);
     }
+
+    public void switchToInputFood(View view) {
+        Intent intent = new Intent(this, inputFood.class);
+        startActivity(intent);
+    }
+
 }
